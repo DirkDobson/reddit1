2d23601945f66919a0c17a0d56ef10f71032e00b
Dirk Dobson <dirkvancouver@live.com>
2018-09-04 12:04:47 -0600

    second model

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,19 +1,58 @@
 class SubsController < ApplicationController
+before_action :set_sub, only: [:show, :edit, :update, :destroy]
+
   def index
+    @subs = Sub.all
+    #render :index
   end
 
   def show
+    #@sub = Sub.find(params[:id])
+    #render :show
   end
 
   def new
+    @sub = Sub.new
+    render partial: "form"
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+
+    if @sub.save
+      redirect_to subs_path
+    else
+      render :new
+    end
   end
 
   def edit
+   # @sub = Sub.find(params[:id])
+   render partial: "form"
   end
 
   def update
+   # @sub = Sub.find(params[:id])
+
+    if @sub.update(sub_params)
+      #redirect_to @sub
+      redirect_to sub_path(@sub)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    #@sub = Sub.find(params[:id])
+    @sub.destroy
+    redirect_to subs_path
+  end
+
+  private
+  def set_sub
+    @sub = Sub.find(params[:id])
+  end
+  def sub_params
+    params.require(:sub).permit(:name)
   end
 end
